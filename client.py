3f73e2e15567e329cbe26abdbaf3e7967921a4f9
MichaelL4438 <michael@lachut.com>
2022-03-17 07:45:39 -0400

    Added password protection

--- client.py
+++ client.py
@@ -5,17 +5,18 @@ import os
 SEPARATOR = "<SEPARATOR>"
 BUFFER_SIZE = 4096
 HOST = "robopi.local"
-port = 5001
+PORT = 5001
+PASSWORD = "password"
 
 def send_apk():
     s = socket.socket()
-    print(f"[+] Connecting to {HOST}:{port}")
-    s.connect((HOST, port))
+    print(f"[+] Connecting to {HOST}:{PORT}")
+    s.connect((HOST, PORT))
     print("[+] Connected to ", HOST)
     filename = "TeamCode-debug.apk"
     filepath = "./TeamCode/build/intermediates/apk/debug/TeamCode-debug.apk"
     filesize = os.path.getsize(filepath)
-    s.send(f"{filename}{SEPARATOR}{filesize}".encode())
+    s.send(f"{filename}{SEPARATOR}{filesize}{SEPARATOR}{PASSWORD}".encode())
 
     progress = tqdm.tqdm(range(filesize), f"Sending {filename}", unit="B", unit_scale=True, unit_divisor=1024)
     with open(filepath, "rb") as f:
