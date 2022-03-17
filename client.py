ddcd84d76f05e2f93b8632bb4eccae7acf2e4402
MichaelL4438 <michael@lachut.com>
2022-03-16 20:58:06 -0400

    Refactored client.py

--- client.py
+++ client.py
@@ -6,23 +6,27 @@ SEPARATOR = "<SEPARATOR>"
 BUFFER_SIZE = 4096
 HOST = "robopi.local"
 
-s = socket.socket()
-host = "robopi.local"
-port = 5001
-print(f"[+] Connecting to {host}:{port}")
-s.connect((host, port))
-print("[+] Connected to ", host)
-filename = "TeamCode-debug.apk"
-filepath = "./TeamCode/build/intermediates/apk/debug/TeamCode-debug.apk"
-filesize = os.path.getsize(filepath)
-s.send(f"{filename}{SEPARATOR}{filesize}".encode())
+def send_apk():
+    s = socket.socket()
+    host = "robopi.local"
+    port = 5001
+    print(f"[+] Connecting to {host}:{port}")
+    s.connect((host, port))
+    print("[+] Connected to ", host)
+    filename = "TeamCode-debug.apk"
+    filepath = "./TeamCode/build/intermediates/apk/debug/TeamCode-debug.apk"
+    filesize = os.path.getsize(filepath)
+    s.send(f"{filename}{SEPARATOR}{filesize}".encode())
 
-progress = tqdm.tqdm(range(filesize), f"Sending {filename}", unit="B", unit_scale=True, unit_divisor=1024)
-with open(filepath, "rb") as f:
-    while True:
-        bytes_read = f.read(BUFFER_SIZE)
-        if not bytes_read:
-            break
-        s.sendall(bytes_read)
-        progress.update(len(bytes_read))
-s.close()
+    progress = tqdm.tqdm(range(filesize), f"Sending {filename}", unit="B", unit_scale=True, unit_divisor=1024)
+    with open(filepath, "rb") as f:
+        while True:
+            bytes_read = f.read(BUFFER_SIZE)
+            if not bytes_read:
+                break
+            s.sendall(bytes_read)
+            progress.update(len(bytes_read))
+    s.close()
+
+if __name__ == "__main__":
+    send_apk()
