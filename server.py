915d6b09b55b4740c5809a0f15eccb1906892276
Michael Lachut <michael@lachut.com>
2022-04-11 16:26:47 -0400

    Update server.py

--- server.py
+++ server.py
@@ -23,11 +23,11 @@ def listen_for_file():
 	progress = tqdm.tqdm(range(filesize), f"Receiving {filename}", unit="B", unit_scale=True, unit_divisor=1024)
 	with open(filename, "wb") as f:
 		while True:
-		bytes_read = client_socket.recv(BUFFER_SIZE)
-		if not bytes_read:
-			break
-		f.write(bytes_read)
-		progress.update(len(bytes_read))
+			bytes_read = client_socket.recv(BUFFER_SIZE)
+			if not bytes_read:
+				break
+			f.write(bytes_read)
+			progress.update(len(bytes_read))
 	client_socket.close()
 	s.close()
 
